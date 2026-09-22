24519493a9c990ddc92353f3fcbd01eb3825d329
Janae Dooley <janaesdooley@gmail.com>
2026-09-22 15:39:11 -0500

    add orderStatus, orderAddressType, ordersTable, orderAddressesTable to schema

---
+++ drizzle/0001_stiff_scarlet_spider.sql
@@ -0,0 +1,26 @@
+CREATE TYPE "public"."order_address_type" AS ENUM('shipping', 'billing');--> statement-breakpoint
+CREATE TYPE "public"."order_status_type" AS ENUM('order received', 'open', 'shipped', 'completed', 'cancelled', 'action required');--> statement-breakpoint
+CREATE TABLE "order_addresses" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"order_id" uuid NOT NULL,
+	"type" "order_address_type" NOT NULL,
+	"name" text,
+	"line1" text NOT NULL,
+	"line2" text,
+	"line3" text,
+	"city" text,
+	"state/province" text,
+	"zipcode" text,
+	"country" text DEFAULT 'US' NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "orders" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"order_number" text NOT NULL,
+	"customer_id" uuid NOT NULL,
+	"status" "order_status_type" DEFAULT 'order received' NOT NULL,
+	CONSTRAINT "orders_order_number_unique" UNIQUE("order_number")
+);
+--> statement-breakpoint
+ALTER TABLE "order_addresses" ADD CONSTRAINT "order_addresses_order_id_orders_id_fk" FOREIGN KEY ("order_id") REFERENCES "public"."orders"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "orders" ADD CONSTRAINT "orders_customer_id_customers_id_fk" FOREIGN KEY ("customer_id") REFERENCES "public"."customers"("id") ON DELETE no action ON UPDATE no action;
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,11 @@
-import { integer, pgTable, varchar, text } from "drizzle-orm/pg-core";
+import {
+  integer,
+  pgTable,
+  pgEnum,
+  varchar,
+  text,
+  uuid,
+} from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -14,4 +21,43 @@ export const customersTable = pgTable("customers", {
   last_name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
   phone: text("phone"),
+});
+
+export const orderStatus = pgEnum("order_status_type", [
+  "order received",
+  "open",
+  "shipped",
+  "completed",
+  "cancelled",
+  "action required",
+]);
+
+export const orderAddressType = pgEnum("order_address_type", [
+  "shipping",
+  "billing",
+]);
+
+export const ordersTable = pgTable("orders", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  order_number: text("order_number").notNull().unique(),
+  customer_id: uuid("customer_id")
+    .notNull()
+    .references(() => customersTable.id),
+  status: orderStatus("status").notNull().default("order received"),
+});
+
+export const orderAddressesTable = pgTable("order_addresses", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  order_id: uuid("order_id")
+    .notNull()
+    .references(() => ordersTable.id),
+  type: orderAddressType("type").notNull(),
+  name: text("name"),
+  line1: text("line1").notNull(),
+  line2: text("line2"),
+  line3: text("line3"),
+  city: text("city"),
+  state: text("state/province"),
+  postal_code: text("zipcode"),
+  country: text("country").notNull().default("US"),
 });
